5c43b4694bf3202db11e67d02e09d6d4ee557cbe
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-30 18:15:19 +0100

    Implement countSongsInNoAlbum Method

--- 103-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 103-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -58,7 +58,9 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public int countSongsInNoAlbum() {
-        return -1;
+        return (int) songs.stream()
+                        .filter(song -> song.getAlbumName().isEmpty())
+                        .count();
     }
 
     @Override

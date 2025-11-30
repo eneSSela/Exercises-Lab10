f22d795bb8297e88d2fdde1852215c3ce3c44011
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-30 17:59:01 +0100

    Implement albumInYear Method

--- 103-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 103-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -43,7 +43,9 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Stream<String> albumInYear(final int year) {
-        return null;
+        return albums.entrySet().stream()
+                    .filter(entry -> entry.getValue() == year)
+                    .map(Map.Entry::getKey);
     }
 
     @Override

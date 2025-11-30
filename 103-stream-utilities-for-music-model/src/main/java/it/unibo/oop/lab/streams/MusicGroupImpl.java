e27fc76a2afe5ac34fa2e96caabb37aa7ce1f70a
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-30 17:57:18 +0100

    Implement albumNames Method

--- 103-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
+++ 103-stream-utilities-for-music-model/src/main/java/it/unibo/oop/lab/streams/MusicGroupImpl.java
@@ -38,7 +38,7 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Stream<String> albumNames() {
-        return null;
+        return albums.keySet().stream();
     }
 
     @Override
